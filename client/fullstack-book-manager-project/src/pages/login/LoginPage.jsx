29b10dbe39bcee266a7f160eae40d6c373078d1a
JoanaMas <joana.mastianica@gmail.com>
2023-04-14 19:09:52 +0300

    3: Login page UI completed reusing components created for register form

--- client/fullstack-book-manager-project/src/pages/login/LoginPage.jsx
+++ client/fullstack-book-manager-project/src/pages/login/LoginPage.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import './loginPage.scss';
+import routes from '../../routes/routes';
+// Components
+import InputField from "../../components/inputFields/InputField";
+import LeftFormSide from "../../components/registerLoginForm/leftSide/LeftFormSide";
+import RightFormSide from "../../components/registerLoginForm/rightSide/RightFormSide";
+import RegisterFormContainer from "../../components/registerLoginForm/registerFormContainer/RegisterFormContainer";
+
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <>
+    <RegisterFormContainer>
+
+    <div className='left'>
+      <RightFormSide buttonText={"Log In"}>
+        <InputField htmlFor={"email"} placeholder={"Email..."} type={"email"} id={"email"}>Email</InputField>
+        <InputField htmlFor={"password"} placeholder={"Password..."} type={"password"} id={"password"}>Password</InputField>
+      </RightFormSide>
+    </div>
+
+      <LeftFormSide
+        title={"Members Log In"}
+        question={'Not a member yet?'}
+        route={routes.registerPage}
+        linkText={"Sign Up."}
+      />
+    </RegisterFormContainer>
+    </>
   );
 };
 
