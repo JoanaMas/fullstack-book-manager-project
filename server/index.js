4fdffc2a4674118496964a0bc03a86e1f1468656
JoanaMas <joana.mastianica@gmail.com>
2023-04-16 15:53:10 +0300

    6: Connected to MongoDB

--- server/index.js
+++ server/index.js
@@ -6,7 +6,12 @@ const mongoose = require("mongoose");
 app.use(cors());
 app.use(express.json());
 
-
+mongoose.connect("mongodb+srv://joanamastianica:root2025@cluster0.l8vzeuj.mongodb.net/?retryWrites=true&w=majority")
+.then(() => {
+  console.log("CONNECT SUCCESS")
+}).catch((err) => {
+  console.log(err)
+})
 
 
 
